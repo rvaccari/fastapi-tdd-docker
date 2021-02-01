105e20e1d8a791d784ee5f02409f573b3fba2281
Vaccari <rvaccari@gmail.com>
2021-02-01 00:04:44 -0300

    Added config Docker database

--- project/app/config.py
+++ project/app/config.py
@@ -2,13 +2,15 @@ import logging
 import os
 from functools import lru_cache
 
-from pydantic import BaseSettings
+from pydantic import BaseSettings, AnyUrl
 
 log = logging.getLogger('uvicorn')
+
 
 class Settings(BaseSettings):
     environment: str = os.getenv('ENVIRONMENT', 'prod')
     testing: bool = os.getenv('TESTING', 1)
+    database_url: AnyUrl = os.environ.get('DATABASE_URL')
 
 
 @lru_cache()
